81865606f1495907e2799b36c9c70e6815a31537
sergei.rylskii <s.rylskyy@gmail.com>
2025-10-14 17:11:50 +0300

    Added allowedHosts config

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     server: {
         host: true,
         port: 8081,
+        allowedHosts: ['.us-east-1.elb.amazonaws.com']
     },
     test: {
         globals: true,
