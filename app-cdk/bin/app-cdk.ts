6d3924c1e7de556a1c3487ccee905021864f6fc2
sergei.rylskii <s.rylskyy@gmail.com>
2025-10-14 15:29:46 +0300

    Added Docker build and push

--- app-cdk/bin/app-cdk.ts
+++ app-cdk/bin/app-cdk.ts
@@ -2,11 +2,14 @@ import * as cdk from 'aws-cdk-lib';
 import { AppCdkStack } from '../lib/app-cdk-stack';
 import {PipelineCdkStack} from "../lib/pipeline-cdk-stack";
 import 'source-map-support/register';
+import { EcrCdkStack } from '../lib/ecr-cdk-stack';
 //#!/usr/bin/env node
 
 const app = new cdk.App();
 
 const testCdkStack = new AppCdkStack(app, 'test', {});
-const pipelineCdkStack = new PipelineCdkStack(app, 'pipeline-stack', {});
-
+const ecrCdkStack = new EcrCdkStack(app, 'ecr-stack', {});
+const pipelineCdkStack = new PipelineCdkStack(app, 'pipeline-stack', {
+    ecrRepository: ecrCdkStack.repository,
+});
 //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
