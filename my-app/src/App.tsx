c33467cbb23141d888f30eca3a30b30c3009b7a7
sergei.rylskii <s.rylskyy@gmail.com>
2025-10-14 17:18:09 +0300

    Fixed Lab4 Logo

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -8,8 +8,7 @@ function App() {
 
   return (
     <>
-        <p>CI/CD Test LAB4</p>
-        <div>
+      <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -18,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <p>CI/CD Test LAB4</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
