9d10e670645c4e161ddf0eda50807f5d867ae600
sergei.rylskii <s.rylskyy@gmail.com>
2025-10-14 17:38:20 +0300

    Added Prod delivery

--- app-cdk/lib/pipeline-cdk-stack.ts
+++ app-cdk/lib/pipeline-cdk-stack.ts
@@ -12,7 +12,6 @@ import * as ecsPatterns from 'aws-cdk-lib/aws-ecs-patterns';
 interface ConsumerProps extends StackProps {
     ecrRepository: ecr.Repository,
     fargateServiceTest: ecsPatterns.ApplicationLoadBalancedFargateService,
-    fargateServiceProd: ecsPatterns.ApplicationLoadBalancedFargateService,
 }
 export class PipelineCdkStack extends Stack {
     constructor(scope: Construct, id: string, props: ConsumerProps) {
@@ -139,21 +138,6 @@ export class PipelineCdkStack extends Stack {
                     input: dockerBuildOutput,
                 }),
             ]
-        });
-        pipeline.addStage({
-            stageName: 'Deploy-Production',
-            actions: [
-                new codepipeline_actions.ManualApprovalAction({
-                    actionName: 'Approve-Deploy-Prod',
-                    runOrder: 1,
-                }),
-                new codepipeline_actions.EcsDeployAction({
-                    actionName: 'Deploy-Fargate-Prod',
-                    service: props.fargateServiceProd.service,
-                    input: dockerBuildOutput,
-                    runOrder: 2,
-                }),
-            ],
         });
         new CfnOutput(this, 'SourceConnectionArn', {
             value: SourceConnection.attrConnectionArn,
--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -17,11 +17,12 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <p>CI/CD Test LAB4</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+
+        <p>Blue/Green Deployment LAB5</p>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
