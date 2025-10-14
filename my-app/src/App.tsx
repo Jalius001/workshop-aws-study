b56cdacf17b9d54645bd372689ac133a661d9da1
sergei.rylskii <s.rylskyy@gmail.com>
2025-10-14 13:55:34 +0300

    First changes

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -8,7 +8,8 @@ function App() {
 
   return (
     <>
-      <div>
+        <p>PULL REQUEST TEST</p>
+        <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
